d635bdd88587d2bd32eaac73dcea4acdfbb9115a
Diana Verevkina <diana_verevkina@mail.ru>
2023-07-01 19:50:27 +0300

    update health test

--- src/__tests__/health.js
+++ src/__tests__/health.js
@@ -1,18 +1,13 @@
 import showHealth from '../showHealth';
 
-// test('cashback at sum: 500', () => {
-//   const result = calculateCashback(500);
-//   expect(result).toBe(0);
-// });
-
-const dataList = [
+test.each([
   ['green', 50, 'healthy'],
   ['yellow', 15, 'wounded'],
   ['red', 10, 'critical'],
-];
-
-const handler = test.each(dataList);
-handler('testing showHealth function with %s indicator and %i health', (_, health, expected) => {
-  const result = showHealth({ health });
-  expect(result).toBe(expected);
-});
+])(
+  ('testing showHealth function with %s indicator and %i health'),
+  (_, health, expected) => {
+    const result = showHealth({ health });
+    expect(result).toBe(expected);
+  },
+);
